617cd66bffea1a4c3c7bafac952d5c2ba7814ca4
Syd Pao <sydpao@outlook.com>
2020-06-18 22:17:30 +0800

    update app names for UPG

--- app/menu.ts
+++ app/menu.ts
@@ -55,17 +55,17 @@ export default class MenuBuilder {
 
   buildDarwinTemplate() {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
+      label: 'Unison Program Generator App',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About the UPG App',
           selector: 'orderFrontStandardAboutPanel:'
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide UPG',
           accelerator: 'Command+H',
           selector: 'hide:'
         },
